846e699a5586a746a768fa035a2df663f6671e69
manik dhupar <manik.dhupar7@gmail.com>
2020-04-13 18:02:52 +0530

    added start end nodes

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ let w = document.querySelector(".main-canvas").offsetWidth;
 let h = 500;
 let blockWidth, blockHeight;
 let drawFlag = false;
+let moveStartNode = false;
+let moveEndNode = false;
+let startNode = document.getElementById("start-node");
+let endNode = document.getElementById("end-node");
+let startNodeCordinates = { x: 150, y: 200 };
+let endNodeCordinates = { x: 900, y: 200 };
+let prevStartNodeFlag = false;
+let prevEndNodeFlag = false;
 
 canvas.width = w - (w % 25);
 canvas.height = h - (h % 25);
@@ -22,32 +30,82 @@ for (let i = 0; i < numberOfRows; i++) {
   visited.push(innerVisited);
 }
 
-canvas.addEventListener("mousedown", e => {
-  drawFlag = true;
-  // let x = e.clientX - canvas.offsetLeft;
-  // let y = e.clientY - canvas.offsetTop;
-  // x = x - (x % 25);
-  // y = y - (y % 25);
-  // x /= 25;
-  // y /= 25;
-  // lastRow.x = x;
-  // lastRow.y = y;
-  drawWall(e);
+canvas.addEventListener("mousedown", (e) => {
   console.log("down");
+  let { x, y } = getGridCordinates(e);
+  x /= 25;
+  y /= 25;
+  if (startNodeCordinates.x === x * 25 && startNodeCordinates.y === y * 25) {
+    console.log("here");
+    moveStartNode = true;
+  } else if (endNodeCordinates.x === x * 25 && endNodeCordinates.y === y * 25) {
+    moveEndNode = true;
+  } else {
+    console.log("inside draw");
+    drawFlag = true;
+    drawWall(e);
+  }
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  console.log("move");
+  let { x, y } = getGridCordinates(e);
+  x /= 25;
+  y /= 25;
+  if (moveStartNode) {
+    if (endNodeCordinates.x === x * 25 && endNodeCordinates.y === y * 25)
+      return;
+    if (startNodeCordinates.x === x * 25 && startNodeCordinates.y === y * 25)
+      return;
+    clearNode(startNodeCordinates);
+    if (prevStartNodeFlag) {
+      drawWall(e, startNodeCordinates);
+      prevStartNodeFlag = false;
+    }
+    if (visited[x][y]) {
+      //clear wall for current node
+      drawWall(e, { x: x * 25, y: y * 25 });
+      prevStartNodeCordinates = { x: x * 25, y: y * 25 };
+      prevStartNodeFlag = true;
+    }
+    startNodeCordinates = { x: x * 25, y: y * 25 };
+    drawStartNode();
+  } else if (moveEndNode) {
+    if (startNodeCordinates.x === x * 25 && startNodeCordinates.y === y * 25)
+      return;
+    if (endNodeCordinates.x === x * 25 && endNodeCordinates.y === y * 25)
+      return;
+    clearNode(endNodeCordinates);
+    if (prevEndNodeFlag) {
+      drawWall(e, endNodeCordinates);
+      prevEndNodeFlag = false;
+    }
+    if (visited[x][y]) {
+      //clear wall for current node
+      drawWall(e, { x: x * 25, y: y * 25 });
+      prevEndNodeCordinates = { x: x * 25, y: y * 25 };
+      prevEndNodeFlag = true;
+    }
+    endNodeCordinates = { x: x * 25, y: y * 25 };
+    drawEndNode();
+  } else if (drawFlag) {
+    if (endNodeCordinates.x === x * 25 && endNodeCordinates.y === y * 25)
+      return;
+    if (startNodeCordinates.x === x * 25 && startNodeCordinates.y === y * 25)
+      return;
+    drawWall(e);
+  } else {
+    return;
+  }
 });
 
 canvas.addEventListener("mouseup", () => {
-  drawFlag = false;
+  (drawFlag = false), (moveStartNode = false), (moveEndNode = false);
   console.log("up");
 });
 
-canvas.addEventListener("mousemove", e => {
-  if (drawFlag) drawWall(e);
-  console.log("move");
-});
-
 canvas.addEventListener("mouseout", () => {
-  drawFlag = false;
+  (drawFlag = false), (moveStartNode = false), (moveEndNode = false);
   console.log("out");
 });
 
@@ -64,11 +122,20 @@ function drawGrid(ctx, w, h) {
   ctx.stroke();
 }
 
-function drawWall(e) {
+function getGridCordinates(e) {
   let x = e.clientX - canvas.offsetLeft;
   let y = e.clientY - canvas.offsetTop;
   x = x - (x % 25);
   y = y - (y % 25);
+  return { x: x, y: y };
+}
+
+function drawWall(e, prevNodeCordinates) {
+  if (prevNodeCordinates) {
+    var { x, y } = prevNodeCordinates;
+  } else {
+    var { x, y } = getGridCordinates(e);
+  }
   if (visited[x / 25][y / 25]) {
     ctx.clearRect(x + 1, y + 1, 23, 23);
     visited[x / 25][y / 25] = false;
@@ -80,5 +147,26 @@ function drawWall(e) {
   ctx.fillStyle = "red";
   ctx.fill();
 }
+
+function drawStartNode() {
+  ctx.drawImage(
+    startNode,
+    startNodeCordinates.x,
+    startNodeCordinates.y,
+    25,
+    25
+  );
+}
+
+function drawEndNode() {
+  ctx.drawImage(endNode, endNodeCordinates.x, endNodeCordinates.y - 1, 25, 25);
+}
+
+function clearNode(cordinates) {
+  ctx.clearRect(cordinates.x + 1, cordinates.y + 1, 23, 23);
+}
+
+drawStartNode();
+drawEndNode();
 
 drawGrid(ctx, w, h);
